07be20c6a2d1ba331b602be09ee6e941607ed94b
Sanyuvi <santhoshh2197@gmail.com>
2024-12-03 19:53:28 +0530

    cors handled

--- index.js
+++ index.js
@@ -14,7 +14,13 @@ const PORT = process.env.PORT;
 const app = express();
 
 //middleware
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:4200/", // Replace with your frontend's URL
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/Images", express.static("Images"));
 
